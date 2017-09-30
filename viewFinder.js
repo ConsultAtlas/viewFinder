dc795f4c5e246837d90e3422ecf1f645cde49b9c
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2017-09-30 12:18:18 -0500

    added search bar

---
+++ viewFinder.js
@@ -0,0 +1,14 @@
+/*
+
+- Use the OpenMovie API Database to get movie information
+and movie post images
+- save movies to a to-watch list
+  - dave it to local storage
+- mark movies as watched
+- give watched moview a thumbs up or down
+
+- we want it to look good
+  - use bootstrap
+  - search box should be an <input> and a <button>, aligned inline
+  - search box results should be displayed as cards
+  - use bootstrap cards and columns
